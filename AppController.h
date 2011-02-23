fe5e402a827bfa3b1f78d69da91dda7bec365fd0
Charles Feduke <charles.feduke@gmail.com>
2011-02-23 07:58:50 -0500

    AWS_ID and gitignore

--- AppController.h
+++ AppController.h
@@ -8,11 +8,17 @@
 
 #import <Cocoa/Cocoa.h>
 
+// AWS_ID.h is a header file that contains only:
+// #define AWS_ID = "a AWS ID here"
+#import "AWS_ID.h"
 
 @interface AppController : NSObject {
 	IBOutlet NSProgressIndicator *progress;
 	IBOutlet NSTextField *searchField;
 	IBOutlet NSTableView *tableView;
+	
+	NSXMLDocument *doc;
+	NSArray *itemNodes;
 }
 
 -(IBAction)fetchBooks:(id)sender;
